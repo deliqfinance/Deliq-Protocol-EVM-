17160cd4cf36e3cf9a61418a1df33a94eec544fa
Deliq Finance <91419086+deliqfinance@users.noreply.github.com>
2021-10-18 17:16:49 +0530

    Update Launchpools.sol

--- contracts/defi-round/Launchpools.sol
+++ contracts/defi-round/Launchpools.sol
@@ -5,7 +5,7 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/utils/SafeCast.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
